fe682108961b54e904a9824909cc933dbf3cc9f3
lyle <lyleokoth.mlengineer@gmail.com>
2026-06-23 15:44:14 +0300

    adds the simple image generator

---
+++ frontend/image-generator/1/js/script.js
@@ -0,0 +1,19 @@
+const API_KEY = ''; // Paste your API Key
+const API_URL = 'https://api.vyro.ai/v2/image/generations';
+
+const imageContainer = document.getElementById('imageContainer');
+const imageResultElement = document.getElementById('imageResult');
+
+// Function to generate the image
+function generateImage() {
+
+}
+
+function setLoadingState(isLoading) {
+
+}
+
+function downloadImage() {
+
+
+}
