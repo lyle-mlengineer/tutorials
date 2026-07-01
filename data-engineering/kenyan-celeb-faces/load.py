d8c4b396eaf77a7fbc06e7012a24b2bb89e28cf8
lyle <lyleokoth.mlengineer@gmail.com>
2026-07-01 18:04:45 +0300

    creates the updated audi labeller

--- data-engineering/kenyan-celeb-faces/load.py
+++ data-engineering/kenyan-celeb-faces/load.py
@@ -10,17 +10,30 @@ ds = load_dataset("parquet", data_dir=snapshot_dir)
 
 base_output_dir = "/home/lyle/datasets/images/kenyan-celeb-faces-original"
 
-for split in ["train", "test", "validation"]:
-    dataset_split = ds[split]
-    split_output_dir = os.path.join(base_output_dir, split)
-    os.makedirs(split_output_dir, exist_ok=True)
+# for split in ["train", "test", "validation"]:
+#     dataset_split = ds[split]
+#     split_output_dir = os.path.join(base_output_dir, split)
+#     os.makedirs(split_output_dir, exist_ok=True)
 
-    for item in dataset_split:
-        image = item["image"]  # This yields a PIL Image object
-        label = item["label"]  # This yields the class label
+#     for item in dataset_split:
+#         image = item["image"]  # This yields a PIL Image object
+#         label = item["label"]  # This yields the class label
         
-        image_name = f"{uuid.uuid4()}.jpg"  # Generate a unique filename
-        output_path = os.path.join(split_output_dir, image_name)
+#         image_name = f"{uuid.uuid4()}.jpg"  # Generate a unique filename
+#         output_path = os.path.join(split_output_dir, image_name)
         
-        image.save(output_path, format="JPEG")  # Save the image in JPEG format
-        print(f"Saved image to {output_path} with label {label}")
+#         image.save(output_path, format="JPEG")  # Save the image in JPEG format
+#         print(f"Saved image to {output_path} with label {label}")
+
+IMAGE_DATA_PATH: str = "/home/lyle/datasets/images"
+CELEB_FACES_PATH: str = os.path.join(IMAGE_DATA_PATH, "kenyan-celeb-faces")
+for split in os.listdir(CELEB_FACES_PATH):
+    split_path = os.path.join(CELEB_FACES_PATH, split)
+    if os.path.isdir(split_path):
+        for image_file in os.listdir(split_path):
+            image_path = os.path.join(split_path, image_file)
+            destination_path = os.path.join(base_output_dir, split, image_file)
+            # copy image
+            image = PIL.Image.open(image_path)
+            image.save(destination_path, format="JPEG")
+            print(f"Saved image to {destination_path}")
